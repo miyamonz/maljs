24ca5cc6d5e5a3711e8fe92adee877fbbf372705
Koichi Miyamoto <m5813k@gmail.com>
2020-05-26 04:12:17 +0900

    print symbol

--- src/maljs/index.test.js
+++ src/maljs/index.test.js
@@ -7,10 +7,16 @@ describe("maljs", () => {
     assert.equal(REP("123.45"), "123.45");
   });
 
+  it("print atom", () => {
+    assert.equal(REP("hoge"), "hoge");
+    assert.equal(REP("nil"), "nil");
+  });
+
   it("parse list", () => {
     assert.equal(REP("()"), "()");
     assert.equal(REP("(1)"), "(1)");
     assert.equal(REP("( 1 5   )"), "(1 5)");
     assert.equal(REP("( 1 4  5   )"), "(1 4 5)");
+    assert.equal(REP("(nil 0 a)"), "(nil 0 a)");
   });
 });
--- src/maljs/printer.js
+++ src/maljs/printer.js
@@ -2,6 +2,9 @@ import { isList } from "./types.js";
 export function pr_str(obj) {
   if (isList(obj)) {
     return `(${obj.map((e) => pr_str(e)).join(" ")})`;
+  } else if (typeof obj === "symbol") {
+    return Symbol.keyFor(obj);
+  } else {
+    return obj.toString();
   }
-  return obj.toString();
 }
